55590e4602f084154b75216f79483d8b7b3f244f
Salko <64722632+sarajlija@users.noreply.github.com>
2023-05-30 15:39:54 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,34 +3,36 @@ function App() {
     <main>
       <section className="section">
         <div className="header">
-          <div className="header-wrapper">
-            <div className="header-title">
-              <div className="header-quote">
-                <p className="quote">
-                  “The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”
-                  <span>
-                    <img src="/assets/desktop/icon-refresh.svg" alt="refresh" />
-                  </span>
-                </p>
-                <small>Ada Lovace</small>
-              </div>
-              <div className="current">
-                <p className="current-description">
-                  <span>
-                    <img src="/assets/desktop/icon-sun.svg" alt="sun" />
-                  </span>
-                  GOOD MORNING, IT’S CURRENTLY
-                </p>
-                <h1 className="current-time">
-                  11:37<span className="span-bst">BST</span>
-                </h1>
-                <h3>IN LONDON UK</h3>
-              </div>
+          <div className="header-quote">
+            <div className="quote-wrapper">
+              <p className="quote">“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”</p>
+              <small>Ada Lovace</small>
             </div>
-            <div className="btn-more">
-              <button className="btn btn-light">more</button>
+            <div className="image-wrapper">
+              <img src="/assets/desktop/icon-refresh.svg" alt="refresh" />
             </div>
           </div>
+          <div className="header-title">
+            <div className="current">
+              <p className="current-description">
+                <span>
+                  <img src="/assets/desktop/icon-sun.svg" alt="sun" />
+                </span>
+                GOOD MORNING, IT’S CURRENTLY
+              </p>
+              <h1 className="current-time">
+                11:37<span className="span-bst">BST</span>
+              </h1>
+              <h3>IN LONDON UK</h3>
+            </div>
+            <div className="btn-more">
+              <button className="btn btn-light">
+                more
+                <img src="/assets/desktop/icon-arrow-up.svg" alt="arrow-down" />
+              </button>
+            </div>
+          </div>
+          {/*<div className="header-wrapper"> </div>*/}
           <div className="description">DESCRIPTION</div>
         </div>
         <img src="/assets/desktop/bg-image-daytime.jpg" alt="daytime" className="image" />
