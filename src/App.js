79c3cef8ccd3b451afff6d8c5424be80734a30ef
Salko <bidzevics@gmail.com>
2023-06-09 15:47:56 +0200

    up

--- src/App.js
+++ src/App.js
@@ -15,15 +15,12 @@ function App() {
     }
   }
   const fetchData = async () => {
-    const response = await fetch(url, options)
-    const dataQuote = await response.json()
-    console.log(dataQuote)
-    setQuote(dataQuote.content)
-    setAutor(dataQuote.originator.name)
     try {
       const response = await fetch(url, options)
-      const dataQuote = await response.text()
+      const dataQuote = await response.json()
       console.log(dataQuote)
+      setQuote(dataQuote.content)
+      setAutor(dataQuote.originator.name)
     } catch (err) {
       setError(err.message)
       setQuote(null)
@@ -35,26 +32,34 @@ function App() {
   /*WORLTIME*/
 
   const [data, setData] = useState(null)
+  const [dateTimeFull, setDateTimeFull] = useState(null)
   const [abbreviation, setAbbreviation] = useState()
   const [timeZone, setTimeZone] = useState(null)
   const [dayOfYear, setDayOfYear] = useState(null)
   const [dayOfWeek, setDayOfWeek] = useState(null)
-  const [weekNumber, setWeekNumber] =useState(null)
+  const [weekNumber, setWeekNumber] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [clientIP, setClientIP] = useState(null)
+  console.log(clientIP)
 
   useEffect(() => {
-    
     const getData = async () => {
       try {
-        const response = await axios.get("http://worldtimeapi.org/api/ip")
-        console.log(response)       
-        setData(response.data.datetime.slice(11, 16))
-        setTimeZone(response.data.timezone)
-        setAbbreviation(response.data.abbreviation)
-        setDayOfYear(response.data.day_of_year)
-        setDayOfWeek(response.data.day_of_week)
-        setWeekNumber(response.data.week_number)
+        const response = await fetch("http://worldtimeapi.org/api/ip")
+        const wdata = await response.json()
+        console.log(wdata)
+        const { timezone, datetime, abbreviation, day_of_year, day_of_week, week_number, client_ip } = wdata
+
+        setData(datetime.slice(11, 16))
+        setDateTimeFull(datetime.slice(0, 10))
+        console.log(dateTimeFull)
+        setTimeZone(timezone)
+        setAbbreviation(abbreviation)
+        setDayOfYear(day_of_year)
+        setDayOfWeek(day_of_week)
+        setWeekNumber(week_number)
+        setClientIP(client_ip)
         setLoading(true)
       } catch (err) {
         setError(err.message)
@@ -70,10 +75,67 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-     fetchData()
-    }, 60000);
-    return () => clearInterval(interval);
-  }, [quote, autor]);
+      fetchData()
+    }, 60000)
+    return () => clearInterval(interval)
+  }, [quote, autor])
+
+  /**LOCATION AND TIME */
+  const [latitude, setLatitude] = useState(null)
+  const [longitude, setLongitude] = useState(null)
+
+  const urlLocation = `http://ip-api.com/json/${clientIP}`
+
+  const fetchLocalTime = async () => {
+    try {
+      const response = await fetch(urlLocation)
+      const ldata = await response.json()
+      const { lon, lat } = ldata
+      console.log(ldata)
+      setLatitude(ldata.lat)
+      setLongitude(ldata.lon)
+      console.log(latitude)
+      console.log(longitude)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchLocalTime()
+  }, [])
+
+  /*SUNSET*/
+  /* date: `${dateTimeFull}`,
+      latitude: `${latitude}`,
+      longitude: `${longitude}`,
+      timeZoneId: `${timeZone}`*/
+  const options3 = {
+    method: "GET",
+    url: "https://sunrise-sunset-times.p.rapidapi.com/getSunriseAndSunset",
+    params: {
+      date: "2021-10-31",
+      latitude: "51.5072",
+      longitude: "-0.1276",
+      timeZoneId: "America/New_York"
+    },
+    headers: {
+      "X-RapidAPI-Key": "02df9e7ffbmshcce54fcdba3ad4bp1c0149jsn5afc4d45cc52",
+      "X-RapidAPI-Host": "sunrise-sunset-times.p.rapidapi.com"
+    }
+  }
+  const fetchSunset = async () => {
+    try {
+      const response = await axios.request(options3)
+      console.log(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  useEffect(() => {
+    fetchSunset()
+  }, [])
+
   /*GEOLOCATION
   const [locationData, setLocationData] = useState(null)
   const [countryCode, setCountryCode] =useState(null)
@@ -102,7 +164,7 @@ console.log(countryCode)
   /*HIDE QUOTE*/
   const [description, setDescription] = useState(false)
   const handleClickDescription = () => {
-    setDescription(!description)    
+    setDescription(!description)
   }
   const handleClickQuote = () => {
     fetchData()
@@ -139,13 +201,14 @@ console.log(countryCode)
               )}
               {loading && <p className="fs-3">Loading....</p>}
               <h3 className="current-timezone">
-                IN <span className="ms-3">{timeZone}</span><span className="ms-3">{}</span>
+                IN <span className="ms-3">{timeZone}</span>
+                <span className="ms-3">{}</span>
               </h3>
             </div>
             <div className="btn-more">
               <button className="btn__more" onClick={handleClickDescription}>
                 <span className="mx-3">more</span>
-                <img src="/assets/desktop/icon-arrow-up.svg" alt="arrow-down" className={`arrow ${description?"active":""}`}/>
+                <img src="/assets/desktop/icon-arrow-up.svg" alt="arrow-down" className={`arrow ${description ? "active" : ""}`} />
               </button>
             </div>{" "}
           </div>{" "}
@@ -167,7 +230,7 @@ console.log(countryCode)
         </div>
       </header>
       <div>
-        <img src="/assets/desktop/bg-image-daytime.jpg" alt="background" className="image" />
+        <img src="/assets/desktop/bg-image-daytime.jpg" alt="background" className="image-desktop" />
       </div>
     </main>
   )
